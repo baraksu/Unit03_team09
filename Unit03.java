86ab2cf10a0369a8e09a408a1060d24ff9561ae9
i5am5cool <ohad.hasson2010@gmail.com>
2025-12-24 19:08:41 +0200

    Update Unit03.java

--- Unit03.java
+++ Unit03.java
@@ -39,14 +39,50 @@ public class Unit03{
     public static int countWords(String s)    {
         return -1;
     }
-    public static int countParts(String s, char separator)    {
-        return -1;
+       public static int countWords(String s){
+        Scanner reader = new Scanner(System.in);
+        int count = 0;
+        String newS = s.replace(" ","");
+        count = s.length() - newS.length() + 1;
+        if (count < 3)
+            return -1;
+        else
+            return count;
     }
-    public static String getParts(String s, char separator)    {
-        return "";
+    
+    public static int countParts(String s, char separator){
+        int firstIndex, lastIndex;
+        firstIndex = s.indexOf(separator);
+        if (firstIndex == -1){
+            if (s.length() > 0)
+                return 1;
+            else
+                return 0;
+        }
+        lastIndex = s.lastIndexOf(separator);
+        if (firstIndex == lastIndex){
+            return 2;
+        }else{
+            return 3;
+        }
     }
-    public static void printParts(String s, char separator)    {
-        
+    
+    public static void printParts(String s, char separator){
+        if (countParts(s,separator) == 1){
+            System.out.println(s);
+        }
+        else if (countParts(s,separator) == 2){
+            int index = s.indexOf(separator);
+            String a = s.substring(0, index);
+            String b = s.substring(index+1);
+            System.out.println(a +"\n" + b);
+        }
+        else if (countParts(s,separator) == 3){
+            int index1 = s.indexOf(separator), index2 = s.lastIndexOf(separator);
+            String a = s.substring(0, index1);
+            String b = s.substring(index1 + 1, index2);
+            String c = s.substring(index2 + 1);
+        }
     } 
     
 }
